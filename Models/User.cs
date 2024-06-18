621d569694e371394c45f2784022670b68769f86
azureuser <github_cloudlabsuser_2108@cloudlabsaiuser.com>
2024-06-18 10:07:20 +0000

    ok

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CRUD_application_2.Models
+﻿namespace CRUD_application_2.Models
 {
     public class User
     {
